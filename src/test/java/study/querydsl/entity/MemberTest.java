c3f0be639d9162dec993740384c14b8e5a2b60a8
hmkim829 <hmkim829@gmail.com>
2022-05-22 02:46:00 +0900

    jpql vs querydsl

---
+++ src/test/java/study/querydsl/entity/MemberTest.java
@@ -0,0 +1,15 @@
+package study.querydsl.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class MemberTest {
+
+    @Autowired
+    EntityManager em;
+}
